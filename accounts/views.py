5ef4b61b5f665658fcb3e50b179c4eae4193c8d0
Jeff Schroeder <jeffschroeder@computer.org>
2010-08-23 21:40:36 -0700

    Fix a syntax error in accounts.views and remove the blocktrans in moderated.html

--- accounts/views.py
+++ accounts/views.py
@@ -70,7 +70,7 @@ def openid_registration(request, template_name='registration/registration_form.h
                 if not settings.MODERATE_NEW_USERS:
                     return HttpResponseNotAllowed(_(u'Disabled account'))
                 else:
-                    return render_to_response("registration/moderated.html", {'user': user,
+                    return render_to_response("registration/moderated.html", {'user': user,},
                                              context_instance=RequestContext(request))
         else:
             return HttpResponseNotAllowed(_(u'Unknown user'))
