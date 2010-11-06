d1f636d49ce22c6f1bea7add133085f7c99a6945
Jeff Schroeder <jeffschroeder@computer.org>
2010-11-06 15:29:36 -0700

    Make everything almost work!

--- site_media/js/uitablefilter-setup.js
+++ site_media/js/uitablefilter-setup.js
@@ -1,4 +1,4 @@
-$(function() { 
+$(function() {
   var theTable = $('table.filter_table')
 
   theTable.find("tbody > tr").find("td:eq(1)").mousedown(function(){
@@ -7,10 +7,17 @@ $(function() {
 
   $("#filter").keyup(function() {
     $.uiTableFilter( theTable, this.value );
+    // Figure out how to show the invite row then
+    if ( $($("#filter_table").attr('rows')).filter(':visible').length == 0 ) {
+      $('#invite').show();
+    }
+    else {
+      $('#invite').hide();
+    }
   })
 
   $('#filter-form').submit(function(){
     theTable.find("tbody > tr:visible > td:eq(1)").mousedown();
     return false;
   }).focus(); //Give focus to input field
-});  
+});
